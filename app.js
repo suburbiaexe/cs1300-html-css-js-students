7f5de7aeb9755b55396597c6ed0030507b4270cf
Elizabeth <elizabeth_c_wu@brown.edu>
2022-09-10 17:36:12 -0400

    bruh

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const apiRequest = async () => {
    */
 
   // TODO fill in your own port number 
-  const PORT_NUMBER = "";
+  const PORT_NUMBER = 8010;
 
   const baseUrl = `http://localhost:${PORT_NUMBER}/proxy/api/`
 
@@ -40,14 +40,20 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15", 
+  const citrus = fruitsArray.filter((fruit) => fruit.genus == "Citrus")
+  console.log(citrus)
 
   // TODO: Create a new HTML element to display your data 
+  const newElt = document.createElement('div');
+  newElt.innerHTML = "citrus fruits: " + JSON.stringify(citrus);
 
   // TODO: Append your new element to the page
+  const existingElt = document.getElementById('cs1300-gallery');
+  existingElt.append(newElt);
 
 }
 
